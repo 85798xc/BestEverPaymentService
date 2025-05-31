a8a1635d465205fdf0a3e5ac1a3bb0c45a3b2a53
Dominykas <105549758+DzikeXV@users.noreply.github.com>
2025-05-31 16:39:08 +0300

    wild card panaikintas

--- src/main/java/com/example/paymentserviceapplication/model/Refund.java
+++ src/main/java/com/example/paymentserviceapplication/model/Refund.java
@@ -2,8 +2,17 @@ package com.example.paymentserviceapplication.model;
 
 import com.example.paymentserviceapplication.converter.RefundStatusConverter;
 import com.example.paymentserviceapplication.enums.RefundStatus;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Convert;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
